f47895a231abafecc2fa0ec22ba8c77656e60718
sbendell <43750640+sbendell@users.noreply.github.com>
2018-10-16 10:20:06 +0100

    Changed values of locks to pointers

--- The Safe Problem 1/CombinationLock.h
+++ The Safe Problem 1/CombinationLock.h
@@ -6,22 +6,19 @@ public:
 	CombinationLock(int size, int Root[]);
 	CombinationLock();
 	~CombinationLock();
-	int GetROOT(const int position) const { return ROOT[position]; }
-	int GetCN(const int position) const { return CN[position]; }
-	int GetLN(const int position) const { return LN[position]; }
-	int GetHN(const int position) const { return HN[position]; }
-	int GetDisplay(const int position) const { return display[position]; }
+	int* GetROOT() const { return ROOT; }
+	int* GetCN() const { return CN; }
+	int* GetLN() const { return LN; }
+	int* GetHN() const { return HN; }
 	int GetSize() const { return size; }
-	void SetROOT(const int value, const int position) { ROOT[position] = value; }
-	void SetCN(const int value, const int position) { CN[position] = value; }
-	void SetLN(const int value, const int position) { LN[position] = value; }
-	void SetHN(const int value, const int position) { HN[position] = value; }
-	void SetDisplay(const int value, const int position) { display[position] = value; }
+	void SetROOT(const int value) { *ROOT = value; }
+	void SetCN(const int value) { *CN = value; }
+	void SetLN(const int value) { *LN = value; }
+	void SetHN(const int value) { *HN = value; }
 private:
 	int size;
-	int ROOT[4];
-	int CN[4];
-	int LN[4];
-	int HN[4];
-	int display[4];
+	int* ROOT;
+	int* CN;
+	int* LN;
+	int* HN;
 };

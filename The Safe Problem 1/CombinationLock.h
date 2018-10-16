d4185f5e8cdd90269dba9f054ff50ae50bb19847
sbendell <43750640+sbendell@users.noreply.github.com>
2018-10-16 10:31:59 +0100

    removed display

--- The Safe Problem 1/CombinationLock.h
+++ The Safe Problem 1/CombinationLock.h
@@ -10,18 +10,15 @@ public:
 	int GetCN(const int position) const { return CN[position]; }
 	int GetLN(const int position) const { return LN[position]; }
 	int GetHN(const int position) const { return HN[position]; }
-	int GetDisplay(const int position) const { return display[position]; }
 	int GetSize() const { return size; }
 	void SetROOT(const int value, const int position) { ROOT[position] = value; }
 	void SetCN(const int value, const int position) { CN[position] = value; }
 	void SetLN(const int value, const int position) { LN[position] = value; }
 	void SetHN(const int value, const int position) { HN[position] = value; }
-	void SetDisplay(const int value, const int position) { display[position] = value; }
 private:
 	int size;
 	int ROOT[4];
 	int CN[4];
 	int LN[4];
 	int HN[4];
-	int display[4];
 };

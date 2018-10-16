edb4bc6ed6845de7a5c4ef9a2d03704f37d477af
sbendell <43750640+sbendell@users.noreply.github.com>
2018-10-16 10:14:38 +0100

    Checked with second lock

--- The Safe Problem 1/The Safe Problem 1.cpp
+++ The Safe Problem 1/The Safe Problem 1.cpp
@@ -30,36 +30,66 @@ void PassHash(CombinationLock& lock, int hash[]) {
 
 int main()
 {
-	int arr[4] = { 6, 7, 8, 9 };
-	CombinationLock firstLock(4, arr);
+	int root[4] = { 6, 7, 8, 9 };
+	CombinationLock firstLock(4, root);
 	int hash[4] = { +1, -1, +1, -1 };
 
 	UnlockHash(firstLock, hash);
 	LockHash(firstLock, hash);
 	PassHash(firstLock, hash);
 
-	cout << "Root: ";
+	cout << "First Lock:\nRoot: ";
 	for (int i = 0; i < firstLock.GetSize(); i++)
 	{
-		cout << firstLock.GetROOT(i);
+		cout << firstLock.GetROOT(i) << " ";
 	}
 
 	cout << "\nCN: ";
 	for (int i = 0; i < firstLock.GetSize(); i++)
 	{
-		cout << firstLock.GetCN(i);
+		cout << firstLock.GetCN(i) << " ";
 	}
 
 	cout << "\nLN: ";
 	for (int i = 0; i < firstLock.GetSize(); i++)
 	{
-		cout << firstLock.GetLN(i);
+		cout << firstLock.GetLN(i) << " ";
 	}
 
 	cout << "\nHN: ";
 	for (int i = 0; i < firstLock.GetSize(); i++)
 	{
-		cout << firstLock.GetHN(i);
+		cout << firstLock.GetHN(i) << " ";
+	}
+
+	int newRoot[4] = { firstLock.GetHN(0), firstLock.GetHN(1), firstLock.GetHN(2), firstLock.GetHN(3) };
+	CombinationLock secondLock(4, newRoot);
+	UnlockHash(secondLock, hash);
+	LockHash(secondLock, hash);
+	PassHash(secondLock, hash);
+
+	cout << "\n\nSecond Lock:\nRoot: ";
+	for (int i = 0; i < secondLock.GetSize(); i++)
+	{
+		cout << secondLock.GetROOT(i) << " ";
+	}
+
+	cout << "\nCN: ";
+	for (int i = 0; i < secondLock.GetSize(); i++)
+	{
+		cout << secondLock.GetCN(i) << " ";
+	}
+
+	cout << "\nLN: ";
+	for (int i = 0; i < secondLock.GetSize(); i++)
+	{
+		cout << secondLock.GetLN(i) << " ";
+	}
+
+	cout << "\nHN: ";
+	for (int i = 0; i < secondLock.GetSize(); i++)
+	{
+		cout << secondLock.GetHN(i) << " ";
 	}
 
 	int x;

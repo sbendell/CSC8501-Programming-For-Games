121c7720984621999389ad9a21599736f8fcb25e
sbendell <43750640+sbendell@users.noreply.github.com>
2018-10-18 13:36:34 +0100

    Commented hash functions in header file

--- The Safe Problem 1/MultiLockSafe.h
+++ The Safe Problem 1/MultiLockSafe.h
@@ -6,9 +6,9 @@ class MultiLockSafe
 public:
 	MultiLockSafe(int Size, int lockSize, int* root, int* UHF, int*LHF, int* PHF);
 	~MultiLockSafe();
-	void UnlockHash(CombinationLock* lock, int* hash);
-	void LockHash(CombinationLock* lock, int* hash);
-	void PassHash(CombinationLock* lock, int* hash);
+	//void UnlockHash(CombinationLock* lock, int* hash);
+	//void LockHash(CombinationLock* lock, int* hash);
+	//void PassHash(CombinationLock* lock, int* hash);
 	template<typename T>
 	void Hash(int whichlock, int* hash,  T &lambda);
 	int GetSize() const { return size; }

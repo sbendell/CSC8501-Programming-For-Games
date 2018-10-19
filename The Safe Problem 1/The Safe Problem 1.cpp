aee6c71ed74929cbd32b6bb98d13e91c90d5e580
sbendell <43750640+sbendell@users.noreply.github.com>
2018-10-19 15:24:40 +0100

    finished

--- The Safe Problem 1/The Safe Problem 1.cpp
+++ The Safe Problem 1/The Safe Problem 1.cpp
@@ -31,11 +31,11 @@ int ArrayToInt(int size, int* arr) {
 	return value;
 }
 
-void OutputHash(int* hash, int size, ofstream& stream) {
+void OutputHash(int* hash, string hashname, int size, ofstream& stream) {
 	for (int i = 0; i < size; i++)
 	{
 		if (i == 0) {
-			stream << "UHF " << hash[i];
+			stream << hashname << hash[i];
 		}
 		else {
 			stream << "," << hash[i];
@@ -56,16 +56,65 @@ void OutputToKeyFile(vector<int>& roots, int lockSize, int* UHF, int* LHF, int* 
 		else {
 			stream << roots[i] << endl;
 		}
-		OutputHash(UHF, lockSize, stream);
-		OutputHash(LHF, lockSize, stream);
-		OutputHash(PHF, lockSize, stream);
+		OutputHash(UHF, "UHF ", lockSize, stream);
+		OutputHash(LHF, "LHF ", lockSize, stream);
+		OutputHash(PHF, "PHF", lockSize, stream);
 	}
 	stream.close();
 }
 
-void ReadFromKeyFile(string filename, ifstream& stream, char& buffer) {
-	stream.open(filename.c_str());
-	
+int ReadKeyFileSize(ifstream& stream) {
+	string str;
+	int temp;
+	stream >> str;
+	stream >> temp;
+	return temp;
+}
+
+void ParseHashes(int* hash, ifstream& stream) {
+	string str;
+	char c;
+	stream >> str;
+	for (int i = 0; i < 4; i++)
+	{
+		if (i == 0) {
+			stream >> hash[i];
+		}
+		else {
+			stream >> c;
+			stream >> hash[i];
+		}
+	}
+}
+
+void ReadSafeFromKeyFile(int* root, int* UHF, int* LHF, int* PHF, ifstream& istream, ofstream& ostream, int iteration) {
+	string str;
+	char c;
+	int tempRoot;
+	for (int i = 0; i < 4; i++)
+	{
+		istream >> c;
+		root[i] = (int)c - 48;
+	}
+	ParseHashes(UHF, istream);
+	ParseHashes(LHF, istream);
+	ParseHashes(PHF, istream);
+	MultiLockSafe readMultiSafe(5, 4, root, UHF, LHF, PHF);
+	ostream << "NS" << iteration << " ";
+	ostream << readMultiSafe;
+}
+
+void ReadFromKeyFile(int* root, int* UHF, int* LHF, int* PHF, string keyfile, string safefile, ifstream& idatafile, ofstream& odatafile){
+	idatafile.open(keyfile.c_str());
+	odatafile.open(safefile.c_str());
+	int keyFileSize = ReadKeyFileSize(idatafile);
+
+	for (int i = 0; i < keyFileSize; i++)
+	{
+		ReadSafeFromKeyFile(root, UHF, LHF, PHF, idatafile, odatafile, i);
+	}
+	idatafile.close();
+	odatafile.close();
 }
 
 int main()
@@ -77,23 +126,25 @@ int main()
 	int* LHF = RandomArray(4, 9, -9);
 	int* PHF = RandomArray(4, 9, -9);
 
-	int iterations = 10000;
+	int iterations = 100000;
 	int safeSize = 5;
 	int lockSize = 4;
 
 	vector<int> roots;
-	roots.reserve(iterations / 20);
+	roots.reserve(iterations);
 	int validLocks = 0;
 	int validLocksBonus = 0;
-	string filename = "key.txt";
-	ofstream datafile;
+	string keyfile = "key.txt";
+	string safefile = "multi-safe.txt";
+	ofstream odatafile;
+	ifstream idatafile;
 
 	for (int i = 0; i < iterations; i++)
 	{
 		MultiLockSafe newSafe(safeSize, lockSize, root, UHF, LHF, PHF);
+		roots.push_back(ArrayToInt(lockSize, root));
 		if (newSafe.IsValid()) {
 			validLocks++;
-			roots.push_back(ArrayToInt(lockSize, root));
 		}
 		if (newSafe.IsValidBonus()) {
 			validLocksBonus++;
@@ -102,10 +153,11 @@ int main()
 		delete[] root;
 		root = RandomArray(lockSize, 9, 0);
 	}
-	OutputToKeyFile(roots, lockSize, UHF, LHF, PHF, filename, datafile);
+	OutputToKeyFile(roots, lockSize, UHF, LHF, PHF, keyfile, odatafile);
+	ReadFromKeyFile(root, UHF, LHF, PHF, keyfile, safefile, idatafile, odatafile);
 
-	cout << "\n\n\n\nValid Multisafes: " << validLocks;
-	cout << "\n\nValid Multisafes Bonus: " << validLocksBonus;
+	cout << "Valid Multisafes: " << validLocks;
+	cout << "\nValid Multisafes Bonus: " << validLocksBonus;
 	clock_t end = clock();
 	cout << "\nTime taken" << double(end - start) / CLOCKS_PER_SEC;
 	int x;
